19f91084f7f2315d4fced7a76b8414245ddb2167
unknown <a8515895@gmail.com>
2020-04-27 00:18:58 +0700

    fix

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,25 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+// $db['default'] = array(
+// 	'dsn'	=> '',
+// 	'hostname' => '127.0.0.1',
+// 	'username' => 'engl_root',
+// 	'password' => '8515895a',
+// 	'database' => 'engl_english',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => true,
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_unicode_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
