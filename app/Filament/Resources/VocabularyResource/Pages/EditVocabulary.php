3b3f68cf29a47d2dd9436988d5522669289bcbc1
khoahuynh <108077424+khoahuynhneezon@users.noreply.github.com>
2023-04-08 18:28:47 +0700

    add button next

--- app/Filament/Resources/VocabularyResource/Pages/EditVocabulary.php
+++ app/Filament/Resources/VocabularyResource/Pages/EditVocabulary.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\VocabularyResource\Pages;
 
 use App\Filament\Resources\VocabularyResource;
 use Filament\Pages\Actions;
+use Filament\Pages\Action;
 use Filament\Resources\Pages\EditRecord;
+
 
 class EditVocabulary extends EditRecord
 {
@@ -14,6 +16,19 @@ class EditVocabulary extends EditRecord
     {
         return [
             Actions\DeleteAction::make(),
+            Actions\Action::make('goToNext')
+                ->label('Next Vocabulary')
+                ->action('goToNext')
+                ->color('success'),
+                $this->getCancelFormAction(),
         ];
+    }
+
+    public function goToNext()
+    {
+        $resources = static::getResource();
+        dd($this->record->id);
+//        $this->redirect($resources::getUrl('create'));
+//        $this->save(true);
     }
 }
