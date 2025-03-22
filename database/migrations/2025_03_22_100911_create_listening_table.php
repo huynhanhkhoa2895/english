c6d24367105e0b02951bb5b6384f75f4eff57052
huynhkhoa <huynhanhkhoa2895@gmail.com>
2025-03-22 23:29:26 +0700

    update

--- database/migrations/2025_03_22_100911_create_listening_table.php
+++ database/migrations/2025_03_22_100911_create_listening_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string("name");
             $table->string("sound")->nullable();
             $table->text("script")->nullable();
-            $table->foreignId('lesson_id')->constrained('lesson')->onDelete('cascade');
             $table->timestamps();
         });
     }
